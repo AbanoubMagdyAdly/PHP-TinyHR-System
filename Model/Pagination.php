eb71ff1eb752294eec81c77487a503876959bc56
Hussien El Attar <hussien.elattar1@gmail.com>
2019-02-10 09:30:47 +0200

    Fix pagination typo

--- Model/Pagination.php
+++ Model/Pagination.php
@@ -30,17 +30,17 @@ class Pagination{
     }
 
     public function handle_url_upper_limit(){
-        if(isset($_GET["page"]) && $_GET["page"] > $this->upper_limit){
-            $_GET["page"] = $upper_limit;
-            $location = "$_SERVER[PHP_SELF]" . "?page=$this->upper_limit";
+        if(isset($_GET["currentpage"]) && $_GET["currentpage"] > $this->upper_limit){
+            $_GET["currentpage"] = $upper_limit;
+            $location = "$_SERVER[PHP_SELF]" . "?currentpage=$this->upper_limit";
             header("location: $location");
         }
     }
 
     public function handle_url_lower_limit(){
-        if(isset($_GET["page"]) && $_GET["page"] < $this->lower_limit ){
-            $_GET["page"] = $lower_limit;
-            $location = "$_SERVER[PHP_SELF]" . "?page=$this->lower_limit";
+        if(isset($_GET["currentpage"]) && $_GET["currentpage"] < $this->lower_limit ){
+            $_GET["currentpage"] = $lower_limit;
+            $location = "$_SERVER[PHP_SELF]" . "?currentpage=$this->lower_limit";
             header("location: $location");
         }
     }
